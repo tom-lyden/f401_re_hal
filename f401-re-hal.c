7a6f1dccc2d459a07ea111635b61f0195833620f
tom-lyden <tomlyden.a@gmail.com>
2026-08-28 19:06:00 +0300

    Initial commit with test function

---
+++ f401-re-hal.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+#include "f401-re-hal.h"
+
+void hello(void)
+{
+	printf("Hello F401-RE HAL test function!\n");
+}
---
+++ f401-re-hal.h
@@ -0,0 +1,6 @@
+#ifndef F401_RE_HAL_LIBRARY_H
+#define F401_RE_HAL_LIBRARY_H
+
+void hello(void);
+
+#endif // F401_RE_HAL_LIBRARY_H
