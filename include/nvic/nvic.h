2c30eea4608623ed01803f934056fb155b9f40cb
tom-lyden <tomlyden.a@gmail.com>
2026-09-16 19:27:46 +0300

    Extraction of EXTI interrupts from NVIC to device header

--- include/nvic/nvic.h
+++ include/nvic/nvic.h
@@ -5,16 +5,7 @@
 #ifndef F401_RE_HAL_NVIC_H
 #define F401_RE_HAL_NVIC_H
 
-typedef enum
-{
-    IRQ_EXTI_LINE_0 = 6,
-    IRQ_EXTI_LINE_1,
-    IRQ_EXTI_LINE_2,
-    IRQ_EXTI_LINE_3,
-    IRQ_EXTI_LINE_4,
-    IRQ_EXTI_LINE_9_5 = 23,
-    IRQ_EXTI_LINE_15_10 = 40,
-} irq_t;
+#include <stm32f401re_interrupts.h>
 
 void NVIC_EnableInterrupt(irq_t irq_n);
 
